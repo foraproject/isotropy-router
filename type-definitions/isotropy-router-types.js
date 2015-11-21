3fe1e5af01ac4f72b3c2e7a3ecef70282a617cbf
Jeswin Kumar <jeswinpk@agilehead.com>
2015-11-21 13:05:50 +0530

    Update type definitions

--- type-definitions/isotropy-router-types.js
+++ type-definitions/isotropy-router-types.js
@@ -1,3 +1,6 @@
-type ContextType = { code: number, redirect: (url: string) => void, method: string, path: string };
+type NextType = () => Promise;
+type OptionsType = { index?: bool | string, root?: string, defer?: bool };
+type ContextType = { code: number, redirect: (url: string) => void, method: string, path: string, status: number };
+type MiddlewareType = (context: ContextType, next: NextType) => Promise;
 type HandlerType = () => Promise;
 type PredicateType = (context: ContextType) => bool;

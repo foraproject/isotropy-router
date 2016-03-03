9a095d1849bde990d31c0c3266a9760583d8f9ac
Jeswin Kumar <jeswinpk@agilehead.com>
2016-03-03 19:26:21 +0530

    WIP: update to shared flow annotations

--- interfaces/parseurl.js
+++ interfaces/parseurl.js
@@ -1,10 +1,16 @@
 declare module "parseurl" {
-  declare type IncomingMessage = {
+  declare interface EventEmitter {
+    on: (eventName: string, cb: Function) => void;
+    removeAllListeners: () => void;
+  }
+
+  declare interface IncomingMessage extends EventEmitter {
     headers: Object;
     httpVersion: string;
     method: string;
+    pipe: (dest: any) => void;
     trailers: Object;
-    setTimeout: (msecs: number, callback: Function) => void;
+    setTimeout: (cb: Function, msec: number) => void;
     statusCode: number;
     url: string;
   }
